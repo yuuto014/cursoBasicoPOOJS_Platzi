80dc4258f30660f1536045bb8de0f42c38d1b5f8
yuuto <larrota.juancamilo@gmail.com>
2022-06-07 09:05:24 -0500

    Uso de clases con RORO ejemplo

--- main.js
+++ main.js
@@ -1,41 +1,87 @@
-//Creacion de un objeto literal
-const natalia = {
-    name: "Natalia",
-    last_name : "Bohorquez",
-    age : 20,
-    cursosAprobados:[
+class Student{
+
+    constructor({
+        name,
+        email,
+        username,
+        twitter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        approvedCourses = [],
+        learingPaths = [],
+    })
+    {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.socialMedia ={
+            twitter,
+            instagram,
+            facebook
+        };
+        this.approvedCourses = approvedCourses;
+        this.learingPaths = learingPaths;
+    }
+
+}
+
+class LearningPath{
+    constructor({
+        name,
+        courses =[],
+    })
+    {
+        this.name = name;
+        this.courses = courses;
+        
+    }
+}
+
+const escuelaWeb = new LearningPath({
+    name: "Escuela de Desarrrollo Web",
+    courses:[
         "Curso Definitivo de HTML y CSS",
-        "Curso Practico de HTML y CSS"
-    ],
-    aprovarCurso(curso){
-        this.cursosAprobados.push(curso)
-        return this.cursosAprobados
-    },
-};
+        "Curso Practico de HTML y CSS",
+        "Curso de Responsive Desing"
+    ]
+});
 
-//Hacer que Natalia apruebe otro curso
-natalia.cursosAprobados.push("Curso de Responsive Design")
+const escuelaData = new LearningPath({
+    name: "Escuela de Data Science",
+    courses:[
+        "Curso DataBussines",
+        "Curso DataVIZ",
+        "Curso Tabnleu"
+    ]
+});
 
-//uso de protopipos
+const escuelaVgs = new LearningPath({
+    name: "Escuela de VideoJuegos",
+            courses:[
+                "Curso Introduccion a la Produccion de VideoJuegos",
+                "Curso de Unreal engine",
+                "Curso de Unity 3D"
+            ]
+});
 
-function Student(name,age,cursosAprobados){
-    this.name =  name;
-    this.age = age;
-    this.cursosAprobados = cursosAprobados;
-    // this.aprovarCurso = function (curso){
-    //     this.cursosAprobados.push(curso);
-    // }
-}
+const juan = new Student({
+    name: "juan Camilo",
+    username: "juanc",
+    email: "juanc@platzi.com",
+    twitter: "juanitoFeliz",
+    learingPaths:[
+        escuelaWeb,
+        escuelaVgs
+    ]
+})
 
-Student.prototype.aprovarCurso = function(curso){
-    this.cursosAprobados.push(curso);
-}
-
-const juanita = new Student (
-    "Juanita Ruiz", 
-    16 , 
-    ["Curso de Introduccion a la Produccion de Videojuegos",
-    "Curso de Creacion de Personajes"]
-);
-
-juanita.aprovarCurso("Curso de Unreal Engine")
+const miguelito = new Student({
+    name: "miguel angelo",
+    username: "miguelito_feliz",
+    email: "miguelito23@platzi.com",
+    instagram: "miguelito_Feliz", 
+    learingPaths:[
+        escuelaWeb,
+        escuelaData
+    ]
+})
